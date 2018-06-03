823da72e5c3a21500d778deef8271c85b535558d
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-03 10:39:07 +0200

    fonction connexion ajoutée a celle d'inscription

--- scriptco.js
+++ scriptco.js
@@ -1,20 +1,29 @@
-window.addEventListener('load',function(){
+/*window.addEventListener('load',function(){
 	document.getElementById('res').reset();
-});
+});*/
 var lab;
-function stocker(){
+function inscription(){
 		if($(':input')[5].checked){
 			lab="Homme"
 		} else {
 			lab='Femme'
 		}
 		if (localStorage.getItem($('#pseudo').val())== null){		
-			console.log("test")
+			alert('Votre inscription s\'est bien passé')
 			var store={"Firstname:": $('#firstname').val(),"lastname":$('#lastname').val(),"Pseudo":$('#pseudo').val(),"password":$('#pass').val(),"Sexe":lab,"Birthdate":$('#date').val(),"city":$('#city').val(),"mail":$('#email').val(),"website":$('#site').val(),"Hobby":$('#hobbies').val(),"phone":$('#tel').val(),"color":$('#color').val()};
 			localStorage.setItem($('#pseudo').val(),JSON.stringify(store))
 		}
-	} else {
-		alert('Formulaire incomplet/Des formats n\'ont pas été respectés')
+	 	else {
+		alert('Formulaire incomplet/Des formats n\'ont pas été respectés ou le pseudo est déjà utilisé')
+	}}
+	function connexion(){
+		if (localStorage.getItem($('#login').val())==null){
+			alert('Nous ne connaissons pas de '+$('#login').val()+" !")
+		} else {
+			if (localStorage.getItem(JSON.parse($('#login').val()).password)== $('#cpass').val()){
+				alert('Vous êtes connecté')
+			}
+		}
 	}
 
 function recuperer(){
